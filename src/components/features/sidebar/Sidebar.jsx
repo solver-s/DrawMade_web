7c9037411cb1b26c2a855cc52208fea9d6632547
funnyhun <funnyhun0222@gmail.com>
2025-10-30 22:14:20 +0900

    feat : 내비게이션 토글 기능 구현 내비게이션의 기본 UI 및 토글 기능을 구현했습니다.
    refs: #3

--- src/components/features/sidebar/Sidebar.jsx
+++ src/components/features/sidebar/Sidebar.jsx
@@ -1,31 +1,43 @@
 import { routes } from "../../../utils/routes";
 import Arrow from "../../../assets/arrow.svg?react";
+import Light from "../../../assets/light.svg?react";
+import Dark from "../../../assets/dark.svg?react";
 
 import { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const Wrapper = styled.nav`
-  position: absolute;
+  position: fixed;
   top: 0;
   z-index: 300;
   height: 100%;
   display: flex;
   flex-direction: column;
-  gap: 1rem;
   border-right: 0.1rem solid ${({ theme }) => theme.borderColor};
+  width: ${({ $isFolded }) => ($isFolded ? "4rem" : "15rem")};
+  transition: width 0.3s ease-in-out;
 `;
 
 const Utils = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 1rem;
+  padding: 1rem 1.2rem;
   border-bottom: 0.1rem solid ${({ theme }) => theme.borderColor};
+
+  & > :first-child {
+    display: ${({ $isFolded }) => ($isFolded ? "none" : "flex")};
+  }
+
+  & > :last-child {
+    rotate: ${({ $isFolded }) => ($isFolded ? "180deg" : "0deg")};
+    transition: rotate 0.2s ease-in-out;
+  }
 `;
 
 const Util = styled.button`
-  display: inline-flex;
-  padding: 0.2rem;
+  display: flex;
+  padding: 0.3rem;
   border-radius: 0.3rem;
   background-color: ${({ theme }) => theme.themeColor};
 
@@ -35,23 +47,59 @@ const Util = styled.button`
 
   & > svg {
     fill: ${({ theme }) => theme.color};
-    width: 0.8rem;
-    height: 0.8rem;
-    rotate: 180deg;
+    width: 0.9rem;
+    height: 0.9rem;
   }
 `;
 
 const List = styled.ul`
-  padding: 1rem;
   list-style-type: none;
   display: flex;
   flex-direction: column;
-  gap: 0.5rem;
+  gap: 0.1rem;
+  padding: 1rem 0.5rem;
+
+  & span {
+    display: ${({ $isFolded }) => $isFolded && "none"};
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  & li {
+    padding: ${({ $isFolded }) => $isFolded && "0.5rem 0rem"};
+    justify-content: ${({ $isFolded }) => $isFolded && "center"};
+  }
 `;
 
 const Item = styled.li`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.5rem 8rem 0.5rem 0.8rem;
+
+  & > span {
+    color: ${({ theme }) => theme.color};
+  }
+
   & > a {
+    position: absolute;
+    width: 100%;
+    height: 100%;
     text-decoration: none;
+    color: transparent;
+  }
+
+  & > svg {
+    flex-shrink: 0;
+    fill: ${({ theme }) => theme.color};
+    width: 1.2rem;
+    height: 1.2rem;
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.borderColor};
+    border-radius: 0.3rem;
   }
 `;
 
@@ -65,20 +113,24 @@ export const Sidebar = () => {
 
   return (
     <Wrapper $isFolded={isFolded}>
-      <Utils>
+      <Utils $isFolded={isFolded}>
         <Util>
-          <Arrow />
+          <Light />
         </Util>
         <Util onClick={handleFold}>
           <Arrow />
         </Util>
       </Utils>
-      <List>
-        <Item>
+      <List $isFolded={isFolded}>
+        <Item key={home.name}>
+          {home.icon}
+          <span>{home.name}</span>
           <Link to={home.path}>{home.name}</Link>
         </Item>
         {home.children.map((child) => (
-          <Item>
+          <Item key={child.name}>
+            {child.icon}
+            <span>{child.name}</span>
             <Link to={child.path}>{child.name}</Link>
           </Item>
         ))}
--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -1,14 +1,18 @@
 import { Children } from "react";
 import { App } from "../components/App";
+import Group from "../assets/group.svg?react";
+import Art from "../assets/art.svg?react";
+import Home from "../assets/home.svg?react";
 
 export const routes = [
   {
     path: "/",
     element: <App />,
     name: "홈",
+    icon: <Home />,
     children: [
-      { path: "/topic", element: <App />, name: "미술 주제" },
-      { path: "/session", element: <App />, name: "세션" },
+      { path: "/topic", element: <App />, name: "미술 주제", icon: <Art /> },
+      { path: "/session", element: <App />, name: "세션", icon: <Group /> },
     ],
   },
 ];
