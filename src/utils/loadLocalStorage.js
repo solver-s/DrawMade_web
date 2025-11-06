90ade7ad06ee8f248b0b722f7ff3a91d6142e773
funnyhun <funnyhun0222@gmail.com>
2025-11-07 05:51:43 +0900

    feat : JSON 이외 타입 지원 1) 기존 loadLocalStorage()는 JSON parse를 기본 로직으로 포함했습니다. 2) 로컬 스토리지에서 불러들인 데이터가 JSON 형태가 아닌 경우 error로 처리했습니다. 3) JSON이 아닌 데이터를 불러들이기 위해 parsing 과정에 대한 에러처리를 분리합니다.
    refs : #2

--- src/utils/loadLocalStorage.js
+++ src/utils/loadLocalStorage.js
@@ -1,9 +1,17 @@
 export const loadLocalStorage = (key) => {
   try {
     const res = window.localStorage.getItem(key);
-    return res ? JSON.parse(res) : null;
+    if (!res) return null;
+
+    // JSON | 일반 문자열
+    try {
+      const parsed = JSON.parse(res);
+      return parsed;
+    } catch (e) {
+      return res;
+    }
   } catch (e) {
-    console.error("로컬 스토리지 데이터를 읽거나 파싱하는 과정에서 오류 발생", error);
+    console.error("로컬스토리지의 데이터 불러오기 실패 :", e);
     return null;
   }
 };
