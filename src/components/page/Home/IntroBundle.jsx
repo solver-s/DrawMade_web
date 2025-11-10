004395cd12cae0e0c764d98b106bb180ae38cad2
funnyhun <funnyhun0222@gmail.com>
2025-11-10 22:07:53 +0900

    feat : IntroBundle 컴포넌트 구현 1) Home 에서 각 서비스의 간략한 정보 및 이동 기능을 제공하는 인트로 번들을 구현합니다. 2) 서비스에 대한 간략한 설명을 제공합니다.
    refs : #7

---
+++ src/components/page/Home/IntroBundle.jsx
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+
+import { Bundle, BorderBox, IconBox } from "../../styled/StyledBox";
+
+import {
+  TopicIcon,
+  SessionIcon,
+  FeedbackIcon,
+  KeywordIcon,
+  SchoolIcon,
+  ShareIcon,
+} from "../../../assets/IconList";
+
+const SmallIconBox = styled(IconBox)`
+  padding: 0.3rem;
+  border-radius: 0.5rem;
+`;
+
+const BoxHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  & > h2 {
+    font-size: 1.1rem;
+  }
+`;
+
+const PointHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  & > svg {
+    width: 0.9rem;
+    height: 0.9rem;
+  }
+
+  & > h3 {
+    font-size: 0.9rem;
+  }
+`;
+
+const Point = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+
+  & > p {
+    margin-left: 1.3rem;
+    font-size: 0.8rem;
+    color: ${({ theme }) => theme.subColor};
+  }
+`;
+
+const BoxInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  margin-bottom: 0.5rem;
+
+  & > p {
+    font-size: 0.9rem;
+    color: ${({ theme }) => theme.subColor};
+  }
+`;
+
+const IntroBox = styled(BorderBox)`
+  gap: 0.5rem;
+`;
+
+export const IntroBundle = () => {
+  const IntroData = [
+    {
+      icon: <TopicIcon />,
+      header: "미술 주제",
+      label: "AI로 입시 미술 주제를 생성해보세요 !",
+      points: [
+        { icon: <SchoolIcon />, header: "대학교 및 학과 맞춤", label: "원하는 대학교를 입력해보세요" },
+        {
+          icon: <KeywordIcon />,
+          header: "키워드 기반 추천",
+          label: "특정 키워드를 입력하여 더 정확한 주제를 받아보세요",
+        },
+      ],
+    },
+    {
+      icon: <SessionIcon />,
+      header: "세션 공유",
+      label: "다른 사람들과 주제를 공유하고 함께 그려보세요 !",
+      points: [
+        { icon: <ShareIcon />, header: "공유 공간", label: "생성한 주제로 세션을 만들고 친구를 초대하세요" },
+        {
+          icon: <FeedbackIcon />,
+          header: "동료 평가",
+          label: "완성한 작품을 업로드하고 피드백을 받아보세요",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <Bundle>
+      {IntroData.map((data) => {
+        return (
+          <IntroBox key={data.header}>
+            <BoxHeader>
+              <SmallIconBox>{data.icon}</SmallIconBox>
+              <h2>{data.header}</h2>
+            </BoxHeader>
+            <BoxInfo>
+              <p>{data.label}</p>
+            </BoxInfo>
+            {data.points.map((point) => {
+              return (
+                <Point key={point.header}>
+                  <PointHeader>
+                    {point.icon}
+                    <h3>{point.header}</h3>
+                  </PointHeader>
+                  <p>{point.label}</p>
+                </Point>
+              );
+            })}
+          </IntroBox>
+        );
+      })}
+    </Bundle>
+  );
+};
