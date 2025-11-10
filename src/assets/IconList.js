ba09c69a89ff77eda5ef67c95f545e72f5786f62
funnyhun <funnyhun0222@gmail.com>
2025-11-10 22:05:23 +0900

    feat : IntroBundle 아이콘 추가 1) Home/IntroBundle 에서 사용되는 아이콘을 추가합니다.
    refs : #7

--- src/assets/IconList.js
+++ src/assets/IconList.js
@@ -1,5 +1,10 @@
 import MainIcon from "./icons/main.svg?react";
 
+import FeedbackIcon from "./icons/feedback.svg?react";
+import KeywordIcon from "./icons/keyword.svg?react";
+import SchoolIcon from "./icons/school.svg?react";
+import ShareIcon from "./icons/share.svg?react";
+
 import ArrowIcon from "./icons/arrow.svg?react";
 import CloseIcon from "./icons/close.svg?react";
 import KaKaoIcon from "./icons/kakao.svg?react";
@@ -11,6 +16,7 @@ import TopicIcon from "./icons/topic.svg?react";
 import SessionIcon from "./icons/session.svg?react";
 
 export { MainIcon };
+export { FeedbackIcon, KeywordIcon, SchoolIcon, ShareIcon };
 export { ArrowIcon, CloseIcon, KaKaoIcon };
 export { DarkIcon, LightIcon };
 export { HomeIcon, TopicIcon, SessionIcon };
